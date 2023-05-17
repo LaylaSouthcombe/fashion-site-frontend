999c3b728440b80a1e7645ad18e012efafb19b5c
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-05-17 22:02:18 +0100

    add in hero home image

--- components/HomeHero.js
+++ components/HomeHero.js
@@ -1,5 +1,21 @@
+import styled from 'styled-components'
+import HomeHeroImage from '../images/HomeHeroImage.jpeg'
+import Image from 'next/image'
+
+const HeroImageDiv = styled.div`
+    width: 100%;
+    height: 40vh;
+    object-fit: contain;
+    overflow: hidden;
+    img {
+        width: 100%;
+    }
+`
+
 export default function HomeHero() {
-    return(
-        <div>Home Hero</div>
+    return (
+        <HeroImageDiv>
+            <Image src={HomeHeroImage} alt="" />
+        </HeroImageDiv>
     )
 }
