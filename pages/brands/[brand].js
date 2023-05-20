36e280a071de86e79c842799ac20b361ff7f24ee
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-05-20 17:11:05 +0100

    add in brand route

---
+++ pages/brands/[brand].js
@@ -0,0 +1,27 @@
+import Header from "@/layout/Header/Header"
+import { mongooseConnect } from "@/lib/mongoose"
+import { Product } from "@/models/Product"
+import ProductsGrid from "@/components/ProductsGrid"
+
+export default function BrandPage({products}){
+    console.log({products})
+    return(
+        <>
+        <Header/>
+        {products?.length ? 
+        <ProductsGrid products={products}/>
+        : null
+        }
+        </>
+    )
+}
+
+export async function getServerSideProps(){
+    await mongooseConnect()
+    const products = await Product.find({}, null, {sort:{'_id': -1}})
+    console.log("products", products)
+    return {
+        props:{
+            products: JSON.parse(JSON.stringify(products))
+    }}
+}
