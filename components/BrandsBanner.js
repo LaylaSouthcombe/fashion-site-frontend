7c0a4df401f6d3640a233ecc768a3e445f6a5a0c
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-05-20 17:54:42 +0100

    add style and functionality to brand banner

--- components/BrandsBanner.js
+++ components/BrandsBanner.js
@@ -1,5 +1,67 @@
+import styled from 'styled-components'
+import adidas from '../images/brandIcons/adidas.png'
+import calvinKlein from '../images/brandIcons/calvinKlein.png'
+import chanel from '../images/brandIcons/chanel.png'
+import essentials from '../images/brandIcons/essentials.png'
+import guess from '../images/brandIcons/guess.png'
+import northFace from '../images/brandIcons/northFace.png'
+import palace from '../images/brandIcons/palace.png'
+import supreme from '../images/brandIcons/supreme.png'
+
+import Image from 'next/image'
 export default function BrandsBanner(){
-    return(
-        <div>Brands banner</div>
+    const brands = [
+        { url: 'adidas', image: adidas},
+        { url: 'calvin-klein', image: calvinKlein},
+        { url: 'chanel', image: chanel},
+        { url: 'essentials', image: essentials},
+        { url: 'guess', image: guess},
+        { url: 'north-face', image: northFace},
+        { url: 'palace', image: palace},
+        { url: 'supreme', image: supreme}
+    ]
+
+    const BrandsContainer = styled.div`
+        width: 90%;
+        margin: 40px auto;
+    `
+    const BrandTitle = styled.h3`
+        padding: 10px 40px;
+        font-weight: normal;
+        font-size: 1.25rem;
+    `
+
+    const BrandsGrid = styled.div`
+        display: grid;
+        grid-template-columns: repeat(8, 1fr);
+        width: 100%;
+        padding: 20px;
+    `
+
+    const BrandBox = styled.a`
+        height: 50px;
+        width: 80%;
+        margin: 0 auto;
+        display: flex;
+        justify-content: center;
+        img {
+                width: auto;
+                height: 100%;
+        }
+    `
+
+    return (
+        <BrandsContainer>
+            <BrandTitle>Brands</BrandTitle>
+            <BrandsGrid>
+                {brands.map((brand, i) => {
+                    return (
+                        <BrandBox key={"brand"+i} href={"brand/" + brand.url}>
+                            <Image src={brand.image} alt={brand.url + " logo"}/>
+                        </BrandBox>
+                    )
+                })}
+            </BrandsGrid>
+        </BrandsContainer>
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,12 @@ import { CheckoutContextProvider } from "@/components/CheckoutContext"
 import {createGlobalStyle} from "styled-components"
 
 const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
   * {
     box-sizing: border-box;
     padding: 0;
     margin: 0;
+    font-family: 'Inter', sans-serif;
   }
 `
 
