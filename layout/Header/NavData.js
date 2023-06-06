9036db616a0368431913389da61a8ffcb0601c76
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-06-06 22:25:25 +0100

    update nav data

--- layout/Header/NavData.js
+++ layout/Header/NavData.js
@@ -9,23 +9,23 @@ const shortNavData = [
         {link: "/clothing/jackets", label: "Jackets"}, 
         {link: "/clothing/trousers", label: "Trousers"}
     ]},
-    { link: "/shoes", label: "Shoes", childLinks: [
-        {link: "/shoes", label: "All shoes"}, 
-        {link: "/shoes/trainers", label: "Trainers"}, 
+    { link: "/shoes/all", label: "Shoes", childLinks: [
+        {link: "/shoes/all", label: "All shoes"}, 
+        {link: "/shoes/sneakers", label: "Sneakers"}, 
         {link: "/shoes/sandals", label: "Sandals"}
     ]},
-    { link: "/bags", label: "Bags", childLinks: [
-        {link: "/bags", label: "All bags"}, 
+    { link: "/bags/all", label: "Bags", childLinks: [
+        {link: "/bags/all", label: "All bags"}, 
         {link: "/bags/rucksack", label: "Rucksacks"}, 
         {link: "/bags/handbags", label: "Handbags"}
     ]},
-    { link: "/jewellery-watches", label: "Jewellry and Watches", childLinks: [
-        {link: "/jewellery-watches", label: "All clothing"}, 
+    { link: "/jewellery-watches/all", label: "Jewellry and Watches", childLinks: [
+        {link: "/jewellery-watches/all", label: "All clothing"}, 
         {link: "/jewellery-watches/jewellery", label: "Jewellery"}, 
         {link: "/jewellery-watches/watches", label: "Watches"}
     ]},
-    { link: "/accessories", label: "Accessories", childLinks: [
-        {link: "/accessories", label: "All accessories"}, 
+    { link: "/accessories/all", label: "Accessories", childLinks: [
+        {link: "/accessories/all", label: "All accessories"}, 
         {link: "/accessories/hats", label: "Hats"}, 
         {link: "/accessories/belts", label: "Belts"}
     ]},
@@ -68,73 +68,68 @@ const navData = {
         ]}
     ]
     },
-    Shoes: { link: "/shoes", label: "Shoes", childLinks: [
-        {title: 'title', listItems: [
-            {link: "/shoes", label: "All shoes"}, 
-            {link: "/shoes/trainers", label: "Trainers"}, 
-            {link: "/shoes/sandals", label: "Sandals"}
+    Shoes: { link: "/shoes/all", label: "Shoes", childLinks: [
+        {title: 'Shoes', listItems: [
+            {link: "/shoes/-t-all", label: "All Shoes"}, 
+            {link: "/shoes/-t-boots", label: "Boots"}, 
+            {link: "/shoes/-t-sandals", label: "Sandals"}
         ]},
-        {title: 'title', listItems: [
-            {link: "/shoes", label: "All shoes"}, 
-            {link: "/shoes/trainers", label: "Trainers"}, 
-            {link: "/shoes/sandals", label: "Sandals"}
+        {title: 'Sneakers', listItems: [
+            {link: "/shoes/-t-sneakers", label: "All Sneakers"}, 
+            {link: "/shoes/-s-running-shoes", label: "Running Shoes"}, 
+            {link: "/shoes/-s-low-top-sneakers", label: "Low Top Sneakers"}
         ]},
-        {title: 'title', listItems: [
-            {link: "/shoes", label: "All shoes"}, 
-            {link: "/shoes/trainers", label: "Trainers"}, 
-            {link: "/shoes/sandals", label: "Sandals"}
+        {title: '_________', listItems: [
+            {link: "/shoes/-s-suede-sneakers", label: "Suede Sneakers"}, 
+            {link: "/shoes/-s-slip-on sneakers", label: "Slip-On Sneakers"}, 
+            {link: "/shoes/-s-high-top-sneakers", label: "High Top Sneakers"}
         ]}
     ]
     },
-    Bags: { link: "/bags", label: "Bags", childLinks: [
-        {title: 'title', listItems: [
-            {link: "/bags", label: "All bags"}, 
-            {link: "/bags/rucksack", label: "Rucksacks"}, 
-            {link: "/bags/handbags", label: "Handbags"}
+    Bags: { link: "/bags/all", label: "Bags", childLinks: [
+        {title: 'Bags', listItems: [
+            {link: "/bags/all", label: "All bags"}, 
+            {link: "/bags/-t-bucket-bags", label: "Bucket Bags"}, 
+            {link: "/bags/-t-tote-bags", label: "Handbags"}
         ]},
-        {title: 'title', listItems: [
-            {link: "/bags", label: "All bags"}, 
-            {link: "/bags/rucksack", label: "Rucksacks"}, 
-            {link: "/bags/handbags", label: "Handbags"}
-        ]},
-        {title: 'title', listItems: [
-            {link: "/bags", label: "All bags"}, 
-            {link: "/bags/rucksack", label: "Rucksacks"}, 
-            {link: "/bags/handbags", label: "Handbags"}
+        {title: '_________', listItems: [
+            {link: "/bags/-t-shoulder-bags", label: "Shoulder Bags"}, 
+            {link: "/bags/-t-cross-body-bags", label: "Cross-body Bags"}, 
+            {link: "/bags/-t-backpacks", label: "Backpacks"}
         ]}
     ]
     },
-    "Jewellry and Watches": { link: "/jewellery-watches", label: "Jewellry and Watches", childLinks: [
+    "Jewellry and Watches": { link: "/jewellery-watches/all", label: "Jewellry and Watches", childLinks: [
         {title: 'title', listItems: [
-            {link: "/jewellery-watches", label: "All clothing"}, 
+            {link: "/jewellery-watches/all", label: "All clothing"}, 
             {link: "/jewellery-watches/jewellery", label: "Jewellery"}, 
             {link: "/jewellery-watches/watches", label: "Watches"}
         ]},
         {title: 'title', listItems: [
-            {link: "/jewellery-watches", label: "All clothing"}, 
+            {link: "/jewellery-watches/all", label: "All clothing"}, 
             {link: "/jewellery-watches/jewellery", label: "Jewellery"}, 
             {link: "/jewellery-watches/watches", label: "Watches"}
         ]},
         {title: 'title', listItems: [
-            {link: "/jewellery-watches", label: "All clothing"}, 
+            {link: "/jewellery-watches/all", label: "All clothing"}, 
             {link: "/jewellery-watches/jewellery", label: "Jewellery"}, 
             {link: "/jewellery-watches/watches", label: "Watches"}
         ]}
     ]
     },
-    "Accessories": { link: "/accessories", label: "Accessories", childLinks: [
+    "Accessories": { link: "/accessories/all", label: "Accessories", childLinks: [
         {title: 'title', listItems: [
-            {link: "/accessories", label: "All accessories"}, 
+            {link: "/accessories/all", label: "All accessories"}, 
             {link: "/accessories/hats", label: "Hats"}, 
             {link: "/accessories/belts", label: "Belts"}
         ]},
         {title: 'title', listItems: [
-            {link: "/accessories", label: "All accessories"}, 
+            {link: "/accessories/all", label: "All accessories"}, 
             {link: "/accessories/hats", label: "Hats"}, 
             {link: "/accessories/belts", label: "Belts"}
         ]},
         {title: 'title', listItems: [
-            {link: "/accessories", label: "All accessories"}, 
+            {link: "/accessories/all", label: "All accessories"}, 
             {link: "/accessories/hats", label: "Hats"}, 
             {link: "/accessories/belts", label: "Belts"}
         ]}
