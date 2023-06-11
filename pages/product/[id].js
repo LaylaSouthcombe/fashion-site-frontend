4db58e3a73a60e4660cc670f7d5c06a0700e441b
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-06-11 10:24:52 +0100

    add in size selection to product page

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -6,6 +6,7 @@ import ProductImagesCarousel from "@/components/ProductImagesCarousel"
 import mongoose from 'mongoose'
 import ProductsCarousel from "@/components/ProductsCarousel/ProductsCarousel"
 import styled from "styled-components"
+import { useState } from "react"
 
 const TopSectionContainer = styled.div`
     display: grid;
@@ -22,8 +23,26 @@ const ProductInformationContainer = styled.div`
 const MoreLikeThisTitle = styled.h2`
     text-align: center;
 `
+
+const SizeButtons = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`
+
+const SizeButton = styled.p`
+    width: fit-content;
+    background-color: ${props => props.isActive ? 'var(--main-dark-blue)' : 'var(--main-light-blue)'};
+    color: ${props => props.isActive ? 'var(--main-light-blue)' : 'var(--main-dark-blue)'};
+    padding: 0.5rem 1rem;
+    margin: 0.25rem;
+    border-radius: 2.5px;
+    font-size: 0.9rem;
+    cursor: pointer;
+`
+
 export default function ProductPage({product, moreLikeThisProducts}){
-    console.log(moreLikeThisProducts)
+    console.log(product)
+    const [selectedSize, setSelectedSize] = useState(product.sizesAndStock[0].size)
     return (
         <>
             <Header/>
@@ -33,7 +52,14 @@ export default function ProductPage({product, moreLikeThisProducts}){
                     <h2>{product.name}</h2>
                     <p>{product.brand}</p>
                     <p>£{product.price}</p>
-
+                    <SizeButtons>
+                        {product.sizesAndStock.map((sizeObject, i) => {
+                            const isActive = selectedSize === sizeObject.size
+                            return (
+                                    <SizeButton key={"sizeObject" + i} isActive={isActive} onClick={() => setSelectedSize(sizeObject.size)}>{sizeObject.size}</SizeButton>
+                            )
+                        })}
+                    </SizeButtons>
                 </ProductInformationContainer>
             </TopSectionContainer>
                 
