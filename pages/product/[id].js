3f32c3b8ecc7cc91e95db0e64384e519fa724431
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-06-11 11:16:06 +0100

    add last stock left para

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -8,6 +8,12 @@ import ProductsCarousel from "@/components/ProductsCarousel/ProductsCarousel"
 import styled from "styled-components"
 import { useState } from "react"
 
+const ProductPathway = styled.p`
+    width: 90%;
+    margin: 0 auto;
+    padding: 0.5rem;
+`
+
 const TopSectionContainer = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr;
@@ -29,7 +35,7 @@ const SizeButtons = styled.div`
     flex-wrap: wrap;
 `
 
-const SizeButton = styled.p`
+const SizeButton = styled.button`
     width: fit-content;
     background-color: ${props => props.isActive ? 'var(--main-dark-blue)' : 'var(--main-light-blue)'};
     color: ${props => props.isActive ? 'var(--main-light-blue)' : 'var(--main-dark-blue)'};
@@ -38,14 +44,49 @@ const SizeButton = styled.p`
     border-radius: 2.5px;
     font-size: 0.9rem;
     cursor: pointer;
+    outline: none;
+    border: none;
+
+    :hover:enabled {
+        background-color:  ${props => props.isActive ? 'var(--main-dark-blue)' : 'var(--main-lightish-blue)'};
+    }
+    
+    :disabled {
+        opacity: 0.5;
+    }
+`
+
+const LastStockLeftPara = styled.div`
+    height: 1.75rem;
+    padding: 0.5rem;
+    span {
+        font-size: 0.8rem;
+    }
+
+    span:first-child{
+        font-weight: bold;
+    }
 `
 
 export default function ProductPage({product, moreLikeThisProducts}){
     console.log(product)
-    const [selectedSize, setSelectedSize] = useState(product.sizesAndStock[0].size)
+
+    const getFirstInStockSize = (product) => {
+        let firstStockInSize
+        for(let i = 0; i < product.sizesAndStock.length; i++){
+            if(product.sizesAndStock[i].stock !== 0 && firstStockInSize === undefined){
+                firstStockInSize = product.sizesAndStock[i]
+            }
+        }
+        return firstStockInSize
+    }
+
+    const [selectedSize, setSelectedSize] = useState(getFirstInStockSize(product))
+
     return (
         <>
             <Header/>
+            <ProductPathway>{product.productCategory} &gt; {product.productType}</ProductPathway>
             <TopSectionContainer>
                 <ProductImagesCarousel images={product.images}/>
                 <ProductInformationContainer>
@@ -54,12 +95,20 @@ export default function ProductPage({product, moreLikeThisProducts}){
                     <p>£{product.price}</p>
                     <SizeButtons>
                         {product.sizesAndStock.map((sizeObject, i) => {
-                            const isActive = selectedSize === sizeObject.size
+                            const isActive = selectedSize.size === sizeObject.size
                             return (
-                                    <SizeButton key={"sizeObject" + i} isActive={isActive} onClick={() => setSelectedSize(sizeObject.size)}>{sizeObject.size}</SizeButton>
+                                    <SizeButton key={"sizeObject" + i} isActive={isActive} onClick={() => setSelectedSize(sizeObject)} disabled={sizeObject.stock === 0}>{sizeObject.size}</SizeButton>
                             )
                         })}
                     </SizeButtons>
+                    <LastStockLeftPara>
+                        {selectedSize.stock === 1 ? 
+                        <>
+                            <span>Last 1 left</span>
+                            <span> - make it yours!</span>
+                        </>
+                        : null}
+                    </LastStockLeftPara>
                 </ProductInformationContainer>
             </TopSectionContainer>
                 
