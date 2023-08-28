062152f0ce0e9c80d33f89cc6b416893fedc32f0
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-08-28 22:48:48 +0100

    add order component

---
+++ components/Order.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+import Image from "next/image"
+
+const OrderContainer = styled.div`
+    width: 80%;
+`
+
+export default function BannerAd({order}) {
+    return (
+        <OrderContainer>
+
+        </OrderContainer>
+    )
+}
