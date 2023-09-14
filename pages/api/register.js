037fbd6577e4a5363999a0bee3a62532a7f4aabc
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-09-14 23:31:32 +0100

    change to passwordHash

--- pages/api/register.js
+++ pages/api/register.js
@@ -23,7 +23,7 @@ export default async function handler(req, res){
 
       const hashedPassword = await bcrypt.hash(password, 10);
 
-      const newUser = new Account({ email, password: hashedPassword, firstName, lastName });
+      const newUser = new Account({ email, passwordHash: hashedPassword, firstName, lastName });
 
       newUser.save()
         .then((account) => {
